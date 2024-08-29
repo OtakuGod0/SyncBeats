1ccad1520a1e52fef0a0c1759f2f7ae548cae511
otaku <otakugod0000@gmail.com>
2024-08-29 18:34:58 +0545

    feature update: playlist sync

--- test/test.py
+++ test/test.py
@@ -1,25 +1,7 @@
-import yt_dlp
-import json
+import os 
 
-def get_video_info(url):
-    # Configure yt-dlp options
-    ydl_opts = {
-        'format': 'best',  # Specify the format of the video to download, 'best' will get the highest quality available
-        'noplaylist': True,  # Only download the video, not the playlist
-        'quiet': True,  # Suppress output except for errors
-        'dump_single_json': True  # Output video information in JSON format
-    }
-    
-    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        # Extract video information
-        video_info = ydl.extract_info(url, download=False)
-    
-    return video_info
+sync_file_path = 'config/Playlistconfig.json'
 
-# Example usage
-video_url = "https://youtu.be/J8Icb6cL0GU?list=PLMGtL2BtbQo76UvooT0ee-AaYD5mq8Kmc"
-info = get_video_info(video_url)
 
-# Print the extracted video information
-with open("test.json", "w") as fp: 
-    json.dump(info, fp)
+
+print(os.path.exists(f'{sync_file_path}'))
